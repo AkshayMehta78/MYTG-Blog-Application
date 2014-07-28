4ce870bdcebe9870ba4031328dd2bd5ecd311d81
Akshay Mehta <akshay.mehta@vit.edu.in>
2014-07-28 16:27:23 +0530

    Added CKEDITOR

--- db/migrate/20140728103641_change_column_typefor_article_table.rb
+++ db/migrate/20140728103641_change_column_typefor_article_table.rb
@@ -1,5 +1,9 @@
 class ChangeColumnTypeforArticleTable < ActiveRecord::Migration
-  def change
+  def up
   		 change_column :Articles, :body, :text , :limit => nil
   end
+  def down
+  		 change_column :Articles, :body, :string , :limit => nil
+  end
+
 end
